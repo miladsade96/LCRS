7536e33813ee6c719e8e86766778964899f36495
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-27 11:09:22 +0430

    Defining and implementing number_of_good_pairs test function

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -13,3 +13,9 @@ def test_is_isomorphic(input_1, input_2, expected):
                          [(5, 2), (10, 2), (28, 3), (354, 4)])
 def test_number_of_one_bits(func_input, expected):
     assert number_of_one_bits(func_input) == expected
+
+
+@pytest.mark.parametrize("func_input, expected",
+                         [([1, 2, 3, 1, 1, 3], 4), ([1, 1, 1, 1], 6), ([1, 2, 3], 0)])
+def test_number_of_good_pairs(func_input, expected):
+    assert number_of_good_pairs(func_input) == expected
