69fd9476aa848e6b793201b61b48b482eb133108
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-27 11:35:25 +0430

    Defining and implementing median_of_two_sorted_arrays tests function

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -19,3 +19,12 @@ def test_number_of_one_bits(func_input, expected):
                          [([1, 2, 3, 1, 1, 3], 4), ([1, 1, 1, 1], 6), ([1, 2, 3], 0)])
 def test_number_of_good_pairs(func_input, expected):
     assert number_of_good_pairs(func_input) == expected
+
+
+@pytest.mark.parametrize("func_input_1, func_input_2, expected",
+                         [([1, 3], [2], 2.0),
+                          ([1, 3], [2, 4], 2.5),
+                          ([256, 0, 9, 70], [597, 263, 85], 85.0),
+                          ([90, 17, 200], [150, 150, 900], 150.0)])
+def test_median_of_two_sorted_arrays(func_input_1, func_input_2, expected):
+    assert median_of_two_sorted_arrays(func_input_1, func_input_2) == expected
