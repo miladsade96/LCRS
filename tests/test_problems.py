e50abcc8de36796748dfe791d15cca5c71a8de4b
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-05-27 11:02:11 +0430

    Defining and implementing number_of_one_bits test function

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -7,3 +7,9 @@ from src.problems.problems import *
                          [("egg", "add", True), ("foo", "bar", False), ("paper", "title", True)])
 def test_is_isomorphic(input_1, input_2, expected):
     assert is_isomorphic(s=input_1, t=input_2) == expected
+
+
+@pytest.mark.parametrize("func_input, expected",
+                         [(5, 2), (10, 2), (28, 3), (354, 4)])
+def test_number_of_one_bits(func_input, expected):
+    assert number_of_one_bits(func_input) == expected
