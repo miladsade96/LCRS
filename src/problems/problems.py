a3bb6732ba2bc2a3b5482fe44974c412bf4ec12d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-05 09:29:28 +0430

    Defining and implementing height_checker_v2 function

--- src/problems/problems.py
+++ src/problems/problems.py
@@ -264,3 +264,25 @@ def height_checker_v1(heights: List[int]) -> int:
         if i != j:
             count += 1
     return count
+
+
+def height_checker_v2(heights: List[int]) -> int:
+    """
+    A school is trying to take an annual photo of all the students. The students are
+    asked to stand in a single file line in non-decreasing order by height. Let this
+    ordering be represented by the integer array expected where expected[i] is the
+    expected height of the ith student in line.
+    You are given an integer array heights representing the current order that the students
+    are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+    Return the number of indices where heights[i] != expected[i].
+    v2 runtime: 20 ms
+    Level of difficulty: Easy
+    :param heights: List of integers
+    :return: The number of indices where heights[i] != expected[i]
+    """
+    sorted_h = sorted(heights)
+    count = 0
+    for i in range(len(heights)):
+        if sorted_h[i] != heights[i]:
+            count += 1
+    return count
