a4c307e02ab4edf6dea9462365ffa96d14114f39
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-06-05 09:29:16 +0430

    Defining and implementing height_checker_v1 function

--- src/problems/problems.py
+++ src/problems/problems.py
@@ -242,3 +242,25 @@ def is_palindrome_v2(x: int) -> bool:
     if x != 0:
         return str(x) == str(x)[::-1]
     return True
+
+
+def height_checker_v1(heights: List[int]) -> int:
+    """
+    A school is trying to take an annual photo of all the students. The students are
+    asked to stand in a single file line in non-decreasing order by height. Let this
+    ordering be represented by the integer array expected where expected[i] is the
+    expected height of the ith student in line.
+    You are given an integer array heights representing the current order that the students
+    are standing in. Each heights[i] is the height of the ith student in line (0-indexed).
+    Return the number of indices where heights[i] != expected[i].
+    v1 runtime: 60 ms
+    Level of difficulty: Easy
+    :param heights: List of integers
+    :return: The number of indices where heights[i] != expected[i]
+    """
+    s = sorted(heights)
+    count = 0
+    for i, j in zip(heights, s):
+        if i != j:
+            count += 1
+    return count
