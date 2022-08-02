3701bacfd146a38883df7b5fd83f0933bcb4c95f
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-02 22:05:22 +0430

    Implementing reverse linked list

--- src/problems/problems.py
+++ src/problems/problems.py
@@ -422,3 +422,20 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+
+
+def reverse_linked_list(head: ListNode) -> ListNode:
+    """
+    Reverse a singly linked list.
+    Level of difficulty: Easy
+    :param head: ListNode
+    :return: ListNode, The reversed linked list
+    """
+    prev = None
+    curr = head
+    while curr:
+        next_node = curr.next
+        curr.next = prev
+        prev = curr
+        curr = next_node
+    return prev
