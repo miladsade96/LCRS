db7fa38ee2922ab7dee3de54ab22dfe884098bdc
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-08-23 11:08:43 +0430

    Implementing merge_two_lists

--- src/problems/problems.py
+++ src/problems/problems.py
@@ -517,3 +517,31 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
+def merge_two_lists(list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+    """
+    You are given the heads of two sorted linked lists list1 and list2
+    Merge the two lists in a one sorted list. The list should be made by
+        splicing together the nodes of the first two lists.
+    Level of difficulty: Easy
+    :param list1: First ListNode
+    :param list2: Second ListNode
+    :return: The head of the merged linked list
+    """
+    dummy = ListNode()
+    tail = dummy
+
+    while list1 and list2:
+        if list1.val < list2.val:
+            tail.next = list1
+            list1 = list1.next
+        else:
+            tail.next = list2
+            list2 = list2.next
+        tail = tail.next
+    if list1:
+        tail.next = list1
+    elif list2:
+        tail.next = list2
+    return dummy.next
