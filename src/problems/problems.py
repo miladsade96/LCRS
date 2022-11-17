f443ed3965c47fc74738a8baff40088d13974edf
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-11-17 12:04:56 +0330

    Implementing product_of_array_except_self

--- src/problems/problems.py
+++ src/problems/problems.py
@@ -579,3 +579,26 @@ def top_k_frequent_elements(nums: List, k: int) -> List[int]:
     sorted_counts = sorted(counts.items(), key=lambda k_v: k_v[1], reverse=True)
     frequents = [sorted_counts[i][0] for i in range(k)]
     return frequents
+
+
+def product_of_array_except_self(nums: List) -> List[int]:
+    """
+    Given an integer array nums, return an array answer such that answer[i] is equal to
+    the product of all the elements of nums except nums[i].
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+    You must write an algorithm that runs in O(n) time and without using the division operation.
+    Level of difficulty: Medium
+    :param nums: List of given integers
+    :return: Product of all the elements of nums except nums[i]
+    """
+    res = [1] * len(nums)
+
+    left_side = 1
+    for i in range(len(nums)):
+        res[i] = left_side
+        left_side *= nums[i]
+    right_side = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= right_side
+        right_side *= nums[i]
+    return res
