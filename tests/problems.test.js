b61bc40ce98b35a895ad2f0c219c7aab8d9c8b50
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-04-30 22:58:26 +0330

    Testing contains Duplicate function

--- tests/problems.test.js
+++ tests/problems.test.js
@@ -4,3 +4,8 @@ test("Testing topKFrequent function", () => {
 	expect(problems.topKFrequent([1, 1, 1, 1, 2, 2, 3], 2)).toEqual([1, 2]);
 	expect(problems.topKFrequent([1, 2, 2, 3, 3, 3, 3, 3], 2)).toEqual([3, 2]);
 });
+
+test("Testing containsDuplicate function", () => {
+	expect(problems.containsDuplicate([1, 2, 3, 4])).toEqual(false);
+	expect(problems.containsDuplicate([1, 2, 3, 1, 4])).toEqual(true);
+});
