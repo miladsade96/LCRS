1871121b24934b0a2db0d4f54db55bbffe34c021
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-04-30 20:18:35 +0330

    Implementing topKFrequent function and exporting it

--- src/problems/problems.js
+++ src/problems/problems.js
@@ -1,1 +1,23 @@
 "use strict";
+
+/**
+ * 347. Top K Frequent Elements
+ * Given an integer array nums and an integer k, return the k most frequent elements.
+ * You may return the answer in any order.
+ * @param nums
+ * @param k
+ * @returns {number[]}
+ */
+function topKFrequent(nums, k) {
+	const counts = {};
+	nums.forEach(num => {
+		if (counts[num]) counts[num]++;
+		else counts[num] = 1;
+	});
+	return Object.entries(counts)
+		.sort((a, b) => (a[1] > b[1] ? -1 : 1))
+		.slice(0, k)
+		.map(e => Number(e[0]));
+}
+
+module.exports = {topKFrequent: topKFrequent};
