ae3e4dc122478f93ca378fa18f35035d3cc44063
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-04-30 20:18:56 +0330

    Testing topKFrequent function

--- tests/problems.test.js
+++ tests/problems.test.js
@@ -1,1 +1,6 @@
 const problems = require("../src/problems/problems");
+
+test("Testing topKFrequent function", () => {
+	expect(problems.topKFrequent([1, 1, 1, 1, 2, 2, 3], 2)).toEqual([1, 2]);
+	expect(problems.topKFrequent([1, 2, 2, 3, 3, 3, 3, 3], 2)).toEqual([3, 2]);
+});
