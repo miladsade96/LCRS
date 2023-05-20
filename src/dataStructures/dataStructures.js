0bbbb8bd44c9e0369af9ea086228624b7d79b297
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-20 18:23:14 +0330

    Implementing doubly linked list data structure

--- src/dataStructures/dataStructures.js
+++ src/dataStructures/dataStructures.js
@@ -156,3 +156,99 @@ class SinglyLinkedList {
 		return this;
 	}
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// Doubly Linked List:
+
+class DoublyNode {
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+		this.prev = null;
+	}
+}
+
+class DoublyLinkedList {
+	constructor(value) {
+		this.head = new DoublyNode(value);
+		this.tail = this.head;
+		this.length = 1;
+	}
+
+	append(value) {
+		const newNode = new DoublyNode(value);
+		newNode.prev = this.tail;
+		this.tail.next = newNode;
+		this.tail = newNode;
+		this.length++;
+		return this;
+	}
+
+	prepend(value) {
+		const newNode = new DoublyNode(value);
+		newNode.next = this.head;
+		this.head.prev = newNode;
+		this.head = newNode;
+		this.length++;
+		return this;
+	}
+
+	print() {
+		const array = [];
+		let currentNode = this.head;
+		while (currentNode !== null) {
+			array.push(currentNode.value);
+			currentNode = currentNode.next;
+		}
+		return array;
+	}
+
+	insert(index, value) {
+		if (index >= this.length) return this.append(value);
+		const newNode = new DoublyNode(value);
+		const leader = this.traverseToIndex(index - 1);
+		const follower = leader.next;
+		leader.next = newNode;
+		newNode.prev = leader;
+		newNode.next = follower;
+		follower.prev = newNode;
+		this.length++;
+		return this;
+	}
+
+	remove(index) {
+		if (this.length === 1) {
+			this.head = null;
+			this.tail = null;
+			this.length--;
+			return this;
+		} else if (index === 0) {
+			this.head = this.traverseToIndex(index + 1);
+			this.head.prev = null;
+			this.length--;
+			return this;
+		} else if (index === this.length - 1) {
+			this.tail = this.traverseToIndex(index - 1);
+			this.tail.next = null;
+			this.length--;
+			return this;
+		} else {
+			const nodeBefore = this.traverseToIndex(index - 1);
+			const nodeAfter = this.traverseToIndex(index + 1);
+			nodeBefore.next = nodeAfter;
+			nodeAfter.prev = nodeBefore;
+			this.length--;
+			return this;
+		}
+	}
+
+	traverseToIndex(index) {
+		let counter = 0;
+		let currentNode = this.head;
+		while (counter !== index) {
+			currentNode = currentNode.next;
+			counter++;
+		}
+		return currentNode;
+	}
+}
