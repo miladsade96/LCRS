ac4abededc2c1172c74082e616db14e0701a7053
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-05-20 18:21:15 +0330

    Implementing hash table data structure

---
+++ src/dataStructures/dataStructures.js
@@ -0,0 +1,56 @@
+// Hash table:
+class HashTable {
+	constructor(size) {
+		this.data = new Array(size);
+	}
+
+	#hash(key) {
+		let hash = 0;
+		for (let i = 0; i < key.length; i++) {
+			hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+		}
+		return hash;
+	}
+
+	set(key, value) {
+		let address = this.#hash(key);
+		if (!this.data[address]) {
+			this.data[address] = [];
+		}
+		this.data[address].push([key, value]);
+		return this.data;
+	}
+
+	get(key) {
+		let address = this.#hash(key);
+		const currentBucket = this.data[address];
+		if (currentBucket) {
+			for (let i = 0; i < currentBucket.length; i++) {
+				if (currentBucket[i][0] === key) {
+					return currentBucket[i][1];
+				}
+			}
+		}
+		return undefined;
+	}
+
+	keys() {
+		const keysArray = [];
+		for (let i = 0; i < this.data.length; i++) {
+			if (this.data[i]) {
+				keysArray.push(this.data[i][0][0]);
+			}
+		}
+		return keysArray;
+	}
+
+	values() {
+		const valuesArray = [];
+		for (let i = 0; i < this.data.length; i++) {
+			if (this.data[i]) {
+				valuesArray.push(this.data[i][0][1]);
+			}
+		}
+		return valuesArray;
+	}
+}
