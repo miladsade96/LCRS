d2ece94d255e3bf747a5eb2da98d8f717de96685
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-06-18 20:01:42 +0330

    Implementing binary search tree class

--- src/dataStructures/dataStructures.js
+++ src/dataStructures/dataStructures.js
@@ -418,3 +418,49 @@ class BSTNode {
 		this.value = value;
 	}
 }
+
+class BinarySearchTree {
+	constructor() {
+		this.root = null;
+	}
+
+	insert(value) {
+		const newNode = new Node(value);
+		if (this.root === null) this.root = newNode;
+		else {
+			let currentNode = this.root;
+			while (true) {
+				if (value < currentNode.value) {
+					// Go to the left side
+					if (!currentNode.left) {
+						currentNode.left = newNode;
+						return this;
+					} else {
+						currentNode = currentNode.left;
+					}
+				} else {
+					// Go to the right side
+					if (!currentNode.right) {
+						currentNode.right = newNode;
+						return this;
+					} else {
+						currentNode = currentNode.right;
+					}
+				}
+			}
+		}
+	}
+
+	lookup(value) {
+		if (!this.root) return false;
+		let currentNode = this.root;
+		while (currentNode) {
+			if (value < currentNode.value) currentNode = currentNode.left;
+			else if (value > currentNode.value) currentNode = currentNode.right;
+			else return currentNode;
+		}
+		return false;
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
