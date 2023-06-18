95c08a50b1c7b4ffd648d2d0d8d04bb33ac86b37
Milad Sadeghi DM <EverLookNeverSee@Proton.Me>
2023-06-18 20:04:18 +0330

    Implementing graph data structure

--- src/dataStructures/dataStructures.js
+++ src/dataStructures/dataStructures.js
@@ -464,3 +464,35 @@ class BinarySearchTree {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+// Graph:
+
+class Graph {
+	constructor() {
+		this.numberOfNodes = 0;
+		this.adjacentList = {};
+	}
+
+	addVertex(node) {
+		this.adjacentList[node] = [];
+		this.numberOfNodes++;
+	}
+
+	addEdge(node1, node2) {
+		// Undirected Graph
+		this.adjacentList[node1].push(node2);
+		this.adjacentList[node2].push(node1);
+	}
+
+	showConnections() {
+		const allNodes = Object.keys(this.adjacentList);
+		for (let node of allNodes) {
+			let nodeConnections = this.adjacentList[node];
+			let connections = "";
+			let vertex;
+			for (vertex of nodeConnections) connections += `${vertex} `;
+			console.log(`${node} --> ${connections}`);
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
